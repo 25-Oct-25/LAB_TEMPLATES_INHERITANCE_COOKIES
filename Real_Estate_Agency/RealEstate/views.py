5695839b38a1a2f7015e7beb9be102fbfd389fe5
abdulmajeed <mjeed.f.b@outlook.sa>
2025-11-03 11:39:02 +0300

    first page style

--- Real_Estate_Agency/RealEstate/views.py
+++ Real_Estate_Agency/RealEstate/views.py
@@ -4,13 +4,18 @@ from django.http import HttpRequest, HttpResponse
 # Create your views here.
 
 def home_view(request : HttpRequest):
-    content = "home"
-    return HttpResponse(content)
+
+    return render(request, "RealEstate/index.html")
 
 def properties_view(request : HttpRequest):
-    content = "properties"
-    return HttpResponse(content)
+    properties = [
+        {"title" : "Villa Modern in Malqa", "image" : "villa1.jpg"},
+        {"title" : "Great home for you in Rimal", "image" : "villa2.jpg"},
+        {"title" : "Villa with 8 bedrooms in Swedey", "image" : "villa3.jpg"},
+        {"title" : "Amazing Villa in Hitten", "image" : "villa4.jpg"},]
+
+    return render(request, "RealEstate/properties.html", context = {"properties" : properties})
 
 def contact_view(request : HttpRequest):
-    content = "contact"
-    return HttpResponse(content)
+
+    return render(request, "RealEstate/contact.html")
