7d771db13384b1d9a98f20f8b90e4cd19a811e49
Abrar <abrar_algh@outlook.sa>
2025-11-04 11:49:31 +0300

    added home, properties, about and contact pages

---
+++ RealEstate/main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+app_name = "main"
+urlpatterns = [path('', views.home_view,name='home'),
+
+               path('properties/', views.properties_view,name='properties'),
+               path('contact/', views.contact_view,name='contact'),
+               path('about/', views.about, name='about'),
+               path('toggle-theme/', views.toggle_theme, name='toggle_theme'),
+               ]
